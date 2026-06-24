ae91f5df41d8493aca090890e1f27784b7aef984
RyukaKuniku <abdullah.rummi@gmail.com>
2026-06-23 21:13:21 -0700

    Add files via upload

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),CK7n)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,d78103bc86cf545bc613f8c7ab7705534a4706c7)
+$(call add-radio-file-sha1-checked,radio/gz.img,a6ad437dc9d1f07b554c4c023c87092c8c754c60)
+$(call add-radio-file-sha1-checked,radio/lk.img,80f7e377f9d9934928e9277e17db24cf19bddcde)
+$(call add-radio-file-sha1-checked,radio/logo.img,b7a4dbf1c00264328dcb7838595b744473c2b033)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,e2e92a7bbec71bf7bf94ae489d327908c874d84c)
+$(call add-radio-file-sha1-checked,radio/md1img.img,e7be54a25721c1a1cdc258e451001f5465ebdb91)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,893531d4a1b061bfaadfb0335135f5765a644ba2)
+$(call add-radio-file-sha1-checked,radio/scp.img,a31c00023e3e003277ebbe1df95123030cdc14b5)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,07f909c693ef612a47707967082b638ec370945d)
+$(call add-radio-file-sha1-checked,radio/sspm.img,1f42dd66bfab3d77b4376bf82f0ddf0ed51faa50)
+$(call add-radio-file-sha1-checked,radio/tee.img,de32255c889ab6f1fafe03400d3ca2b51a4b13cf)
+$(call add-radio-file-sha1-checked,radio/tkv.img,ef7d8483569a423a0d540d44c4a5593a4f81150b)
+
+endif
